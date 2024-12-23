fec9cf733372b630ee76da763cc1670fa2eff4eb
Maryama Mohamed <queenikhlaas50@gmail.com>
2024-12-22 23:12:12 -0300

    Flex widget

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: prefer_const_constructors, avoid_print, sort_child_properties_last
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          title: Text('Flex widget Lesson'),
+        ),
+        body: Flex(
+          direction: Axis.horizontal,
+          children: [
+            
+            Container(
+              
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.purple,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
